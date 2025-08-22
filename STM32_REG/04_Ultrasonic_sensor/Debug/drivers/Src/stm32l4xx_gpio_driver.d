bcb64179924504eca8e68c1fd7bfa93ebe8ac17d
Ashwin <ashwinkumarh147@gmail.com>
2025-08-22 11:13:23 +0530

    add ultrasonic

---
+++ STM32_REG/04_Ultrasonic_sensor/Debug/drivers/Src/stm32l4xx_gpio_driver.d
@@ -0,0 +1,6 @@
+drivers/Src/stm32l4xx_gpio_driver.o: \
+ ../drivers/Src/stm32l4xx_gpio_driver.c \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/04_Ultrasonic_sensor/drivers/Inc/stm32l4xx_gpio_driver.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/04_Ultrasonic_sensor/drivers/Inc/stm32l4xx.h
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/04_Ultrasonic_sensor/drivers/Inc/stm32l4xx_gpio_driver.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/04_Ultrasonic_sensor/drivers/Inc/stm32l4xx.h:
