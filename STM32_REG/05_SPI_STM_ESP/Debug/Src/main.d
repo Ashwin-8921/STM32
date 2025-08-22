0f558fe0f51a96436ce29af2aef690013d2eb1f8
Ashwin <ashwinkumarh147@gmail.com>
2025-08-22 17:13:47 +0530

    add spi file

---
+++ STM32_REG/05_SPI_STM_ESP/Debug/Src/main.d
@@ -0,0 +1,11 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/05_Esp32_SPI/drivers/Inc/stm32l4xx.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/05_Esp32_SPI/drivers/Inc/stm32l4xx_gpio_driver.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/05_Esp32_SPI/drivers/Inc/stm32l4xx.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/05_Esp32_SPI/drivers/Inc/stm32l4xx_spi_driver.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/05_Esp32_SPI/drivers/Inc/stm32l4xx_gpio_driver.h
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/05_Esp32_SPI/drivers/Inc/stm32l4xx.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/05_Esp32_SPI/drivers/Inc/stm32l4xx_gpio_driver.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/05_Esp32_SPI/drivers/Inc/stm32l4xx.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/05_Esp32_SPI/drivers/Inc/stm32l4xx_spi_driver.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/05_Esp32_SPI/drivers/Inc/stm32l4xx_gpio_driver.h:
---
+++ STM32_REG/05_SPI_STM_ESP/Debug/drivers/Src/stm32l4xx_spi_driver.d
@@ -0,0 +1,9 @@
+drivers/Src/stm32l4xx_spi_driver.o: ../drivers/Src/stm32l4xx_spi_driver.c \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/05_Esp32_SPI/drivers/Inc/stm32l4xx_spi_driver.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/05_Esp32_SPI/drivers/Inc/stm32l4xx.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/05_Esp32_SPI/drivers/Inc/stm32l4xx_gpio_driver.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/05_Esp32_SPI/drivers/Inc/stm32l4xx_spi_driver.h
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/05_Esp32_SPI/drivers/Inc/stm32l4xx_spi_driver.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/05_Esp32_SPI/drivers/Inc/stm32l4xx.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/05_Esp32_SPI/drivers/Inc/stm32l4xx_gpio_driver.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/05_Esp32_SPI/drivers/Inc/stm32l4xx_spi_driver.h:
