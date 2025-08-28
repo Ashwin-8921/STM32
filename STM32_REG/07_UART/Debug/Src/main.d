1204a3d727e6fea1a3386053ff7ad9a68858dfee
Ashwin <ashwinkumarh147@gmail.com>
2025-08-28 18:26:58 +0530

    add uart file

---
+++ STM32_REG/07_UART/Debug/Src/main.d
@@ -0,0 +1,17 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/stm_drivers/drivers/inc/stm32l4xx.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/stm_drivers/drivers/inc/stm32l4xx_gpio_driver.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/stm_drivers/drivers/inc/stm32l4xx.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/stm_drivers/drivers/inc/stm32l4xx_spi_driver.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/stm_drivers/drivers/inc/stm32l4xx_i2c_driver.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/stm_drivers/drivers/inc/stm32l4xx_usart_driver.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/stm_drivers/drivers/inc/stm32l4xx_gpio_driver.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/stm_drivers/drivers/inc/stm32l4xx_usart_driver.h
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/stm_drivers/drivers/inc/stm32l4xx.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/stm_drivers/drivers/inc/stm32l4xx_gpio_driver.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/stm_drivers/drivers/inc/stm32l4xx.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/stm_drivers/drivers/inc/stm32l4xx_spi_driver.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/stm_drivers/drivers/inc/stm32l4xx_i2c_driver.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/stm_drivers/drivers/inc/stm32l4xx_usart_driver.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/stm_drivers/drivers/inc/stm32l4xx_gpio_driver.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/stm_drivers/drivers/inc/stm32l4xx_usart_driver.h:
