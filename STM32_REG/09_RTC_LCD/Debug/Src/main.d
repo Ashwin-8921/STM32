0fd76e30b0c8c3d1b5e6c5400bf4fd0a044a927f
Ashwin <ashwinkumarh147@gmail.com>
2025-08-29 19:12:38 +0530

    add rtc files

---
+++ STM32_REG/09_RTC_LCD/Debug/Src/main.d
@@ -0,0 +1,21 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/09_RTC_LCD/drivers/inc/stm32l4xx.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/09_RTC_LCD/drivers/inc/stm32l4xx_gpio_driver.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/09_RTC_LCD/drivers/inc/stm32l4xx.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/09_RTC_LCD/drivers/inc/stm32l4xx_spi_driver.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/09_RTC_LCD/drivers/inc/stm32l4xx_i2c_driver.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/09_RTC_LCD/drivers/inc/stm32l4xx_usart_driver.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/09_RTC_LCD/drivers/inc/RTC.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/09_RTC_LCD/drivers/inc/LCD.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/09_RTC_LCD/drivers/inc/RTC.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/09_RTC_LCD/drivers/inc/LCD.h
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/09_RTC_LCD/drivers/inc/stm32l4xx.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/09_RTC_LCD/drivers/inc/stm32l4xx_gpio_driver.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/09_RTC_LCD/drivers/inc/stm32l4xx.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/09_RTC_LCD/drivers/inc/stm32l4xx_spi_driver.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/09_RTC_LCD/drivers/inc/stm32l4xx_i2c_driver.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/09_RTC_LCD/drivers/inc/stm32l4xx_usart_driver.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/09_RTC_LCD/drivers/inc/RTC.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/09_RTC_LCD/drivers/inc/LCD.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/09_RTC_LCD/drivers/inc/RTC.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/09_RTC_LCD/drivers/inc/LCD.h:
