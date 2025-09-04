2cb7c3d9d766db0217ee660385b42a4d62cda5ea
Ashwin <ashwinkumarh147@gmail.com>
2025-09-04 14:29:24 +0530

    add systick interrupt files

---
+++ STM32_REG/15_Systick_Interrupt/drivers/inc/stm32l4xx_systick_driver.h
@@ -0,0 +1,10 @@
+
+
+#ifndef INC_STM32L4XX_SYSTICK_DRIVER_H_
+#define INC_STM32L4XX_SYSTICK_DRIVER_H_
+
+void systickDelayMs(int delay);
+void systick_1hz_Interrupt(void);
+
+
+#endif /* INC_STM32L4XX_SYSTICK_DRIVER_H_ */
