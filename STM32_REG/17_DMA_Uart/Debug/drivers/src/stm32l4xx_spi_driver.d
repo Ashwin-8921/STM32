2b2de280da727be052f6c60461e2681e0e128e1a
Ashwin <ashwinkumarh147@gmail.com>
2025-09-04 18:36:11 +0530

    add dma file

---
+++ STM32_REG/17_DMA_Uart/Debug/drivers/src/stm32l4xx_spi_driver.d
@@ -0,0 +1,13 @@
+drivers/src/stm32l4xx_spi_driver.o: ../drivers/src/stm32l4xx_spi_driver.c \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/DMA_Uart/drivers/inc/stm32l4xx_spi_driver.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/DMA_Uart/drivers/inc/stm32l4xx.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/DMA_Uart/drivers/inc/stm32l4xx_gpio_driver.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/DMA_Uart/drivers/inc/stm32l4xx_spi_driver.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/DMA_Uart/drivers/inc/stm32l4xx_i2c_driver.h \
+ C:/Users/USER/STM32CubeIDE/workspace_1.19.0/DMA_Uart/drivers/inc/stm32l4xx_usart_driver.h
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/DMA_Uart/drivers/inc/stm32l4xx_spi_driver.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/DMA_Uart/drivers/inc/stm32l4xx.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/DMA_Uart/drivers/inc/stm32l4xx_gpio_driver.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/DMA_Uart/drivers/inc/stm32l4xx_spi_driver.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/DMA_Uart/drivers/inc/stm32l4xx_i2c_driver.h:
+C:/Users/USER/STM32CubeIDE/workspace_1.19.0/DMA_Uart/drivers/inc/stm32l4xx_usart_driver.h:
---
+++ STM32_REG/STM32_DRIVER_FILES/stm32l4xx_systick_driver.h
@@ -0,0 +1,10 @@
+
+
+#ifndef INC_STM32L4XX_SYSTICK_DRIVER_H_
+#define INC_STM32L4XX_SYSTICK_DRIVER_H_
+
+void systickDelayMs(int delay);
+void timer2_1hz_interrupt_init(void);
+
+
+#endif /* INC_STM32L4XX_SYSTICK_DRIVER_H_ */
